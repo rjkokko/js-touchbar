dc9b5204a9c02b8fb4e5ac19118f6f0dfc71a018
Riku Kokko <riku.kokko@futurice.com>
2019-03-08 23:58:14 +0200

    Add {}|/ to touch bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,44 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+
+function insertTextIntoDocument(text: string): boolean {
+  let editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return false; // No open text editor
+  }
+  if (editor !== undefined) {
+    let insertPosition: vscode.Position = editor.selection.active;
+    editor.edit(edit => {
+      edit.insert(insertPosition, text);
+    });
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function registerCommand(context: vscode.ExtensionContext, char: string) {
+  let disposable = vscode.commands.registerCommand(`js-touch.${char}`, () => {
+    // The code you place here will be executed every time your command is executed
+
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return; // No open text editor
+    }
+    insertTextIntoDocument(char);
+  });
+
+  context.subscriptions.push(disposable);
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "js-touch" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+  registerCommand(context, "{");
+  registerCommand(context, "}");
+  registerCommand(context, "\\");
+  registerCommand(context, "|");
 }
 
 // this method is called when your extension is deactivated
